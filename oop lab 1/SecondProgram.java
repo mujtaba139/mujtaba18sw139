a6673c67cd66d8f17ba4480a9845d8917d81c1ac
mujtaba139 <49727246+mujtaba139@users.noreply.github.com>
2019-04-17 10:13:12 -0700

    Add files via upload

---
+++ oop lab 1/SecondProgram.java
@@ -0,0 +1,13 @@
+ class SecondProgram 
+ { 
+ public static void main(String args[]) 
+ { 
+   double a=50.25; 
+   System.out.println("Double="+a); 
+   int b=(int)a; 
+   System.out.println("Integer="+b); 
+   float c=(float)(a-b); 
+   System.out.println("Float="+c); 
+ } 
+ } 
+
---
+++ oop lab 1/program.java
@@ -0,0 +1,18 @@
+class Program{ 
+ public static void main(String args[]) 
+      { 
+         String name="Mujtaba"; 
+         double percent=85.95; 
+         int tmarks=410; 
+         char grade='A'; 
+         boolean status=true; 
+ System.out.println(name); 
+ System.out.println(percent); 
+System.out.println(tmarks); 
+ System.out.println(grade); 
+if(status==true){ 
+ System.out.println("Pass");} 
+else{ 
+system.out.println("Fail");} 
+      } 
+ } 
